bdfc3a9b8fd041a961201c92b100f36d63b63ec7
choir24 <richardchoi54@gmail.com>
2023-06-28 20:21:35 -0400

    display all current users of application for admin only

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,7 @@
-import React, {useState, useEffect} from "react"
+import React, {useState, useEffect, useMemo, ReactNode} from "react"
 import {Client, Account, ID} from "appwrite"
 import {toast} from "react-toastify"
+import axios from "axios"
 import api from "../api/api"
 import {ButtonSubmit} from "../components/Button"
 
@@ -40,28 +41,58 @@ export function Input(props: InputTypes):React.JSX.Element{
   return(<input type = {props.type} onChange = {(e)=>props.onChange(e.target.value)} placeholder = {props.placeholder}/>)
 }
 
+export async function GetAccount(setUser: (e:User)=>void){
+  try{
+    const user = await api.getAccount();
+    setUser(user);
+  }catch(err){
+    console.error(err);
+  }
+}
+
+export async function GetUsers(setListOfUsers: (e:User[])=>void){
+  try{
+    const [dataResponse] = await Promise.all([
+      axios.get("https://car-app-backend-0ejb.onrender.com/getUsers")
+    ])
+    if(dataResponse.data.users.length){
+      setListOfUsers(dataResponse.data.users);
+    }
+  }catch(err){
+    console.error(err)
+  }
+}
+
+export function DisplayUsers(listOfUsers: User[]){
+  return listOfUsers.map((user:User)=>{
+    return(
+    <ul>
+      <li>{user.email}</li>
+      <li>{user.$createdAt}</li>
+      <li>{user.$updatedAt? user.$updatedAt : ""}</li>
+      <li>{user.name}</li>
+    </ul>
+    )
+  })
+}
+
 export default function Login(){
 
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [name, setName] = useState<string>("");
   const [user, setUser] = useState<User>();
+  const [listOfUsers, setListOfUsers] = useState<User[]>([])
 
   useEffect(()=>{
-    
-    async function getAccount(){
-      try{
-        const user = await api.getAccount();
-        setUser(user);
-      }catch(err){
-        console.error(err);
-      }
-    }
-
-    getAccount()
+    GetAccount((e:User) => setUser(e))
   },[])
 
-  async function handleSignUp(){
+  useMemo(()=>{
+    GetUsers((e:User[])=>setListOfUsers(e));
+  },[])
+
+  async function handleSignUp():Promise<void>{
     try{
 
       if(!email){
@@ -115,7 +146,7 @@ export default function Login(){
   }
   }
 
-  async function handleLogin(){
+  async function handleLogin(): Promise<void>{
     try{
       if(!email){
         toast.error("Please input an email address");
@@ -151,7 +182,7 @@ export default function Login(){
     }
   }
 
-  async function handleLogout(){
+  async function handleLogout(): Promise<void>{
     try{
       const user = await api.deleteCurrentSession();
       console.log(user);
@@ -161,44 +192,41 @@ export default function Login(){
     }
   }
 
-
-  async function test(){
-    console.log('test')
-  }
-
-  
   return(
     <main>
-      <button onClick = {()=>{
-        test()
-      }}>Test</button>
+        {user? <h1>Welcome {user.name}</h1> : <h1>Login</h1>}
 
-      {user? <h1>Welcome {user.name}</h1> : <h1>Login</h1>}
+        {user ? 
+        ButtonSubmit({handleButtonClick: (e:React.MouseEvent<HTMLButtonElement, MouseEvent>)=>handleLogout(), text: "Logout"})
+
+        :
+        <form>
+          {ButtonSubmit({handleButtonClick: (e:React.MouseEvent<HTMLButtonElement, MouseEvent>)=>handleLogin(), text: "Login"})}
+          {ButtonSubmit({handleButtonClick: (e:React.MouseEvent<HTMLButtonElement, MouseEvent>)=>handleSignUp(), text: "SignUp"})}
+          {/* <Button handleButtonClick = {()=>getAccount()} text = {"Get Account"}/> */}
+        
+          {Input({type: "string", onChange: (e)=>setEmail(e), placeholder: "Your Email"})}
+          {Input({type: "string", onChange: (e)=>setName(e), placeholder: "Your Full Name"})}
+          {Input({type: "string", onChange: (e)=>setPassword(e), placeholder: "Your Password"})}
+        </form>
+        }
+
 
       {user ? 
-      <ButtonSubmit handleButtonClick = {(e:React.MouseEvent<HTMLButtonElement, MouseEvent>)=>handleLogout()} text = {"Logout"}/>
-      :
-      <form>
-        <ButtonSubmit handleButtonClick = {(e:React.MouseEvent<HTMLButtonElement, MouseEvent>)=>handleLogin()} text = {"Login"}/>
-        <ButtonSubmit handleButtonClick = {(e:React.MouseEvent<HTMLButtonElement, MouseEvent>)=>handleSignUp()} text = {"SignUp"}/>
-        {/* <Button handleButtonClick = {()=>getAccount()} text = {"Get Account"}/> */}
-
-        <Input type = "string" onChange = {(e)=>setEmail(e)} placeholder = "Your Email"/>
-        <Input type = "string" onChange = {(e)=>setName(e)} placeholder = "Your Full Name"/>
-        <Input type = "password" onChange = {(e)=>setPassword(e)} placeholder = "Your Password"/>
-      </form>
-      }
-
-      {user?.$id === "649c8a408d41d5c02f5c" ? 
+      user?.$id === "649c8a408d41d5c02f5c" ? 
         <section>
           <h3>Admin Hub</h3>
+          {DisplayUsers(listOfUsers)}
         </section>
       :
         <section>
           <h3>Employee Hub</h3>
-
         </section>
-      }
+      
+      :
+      ""
+    }
+
     </main>
   )
 }
