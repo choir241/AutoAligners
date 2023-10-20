4ba4515aab06975b7ca9bc54a91d3230791721c1
choir24 <richardchoi54@gmail.com>
2023-10-20 15:21:18 -0400

    start implement Zustand state management, move page lazy loading & default object values for defaultEmployee & defaultUser to separate page, remove unnecessary properties from Context page and App

---
+++ src/middleware/Types.tsx
@@ -0,0 +1,10 @@
+import {ClientFinance, Profile, PTO, InventoryItem, CartItem, Appointment, PurchasedItem, Estimate, User} from "./Interfaces.tsx"
+
+type CarState = {
+}
+
+type CarAction = {
+}
+
+export type State = {
+}
