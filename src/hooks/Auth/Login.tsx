10718c9a09206a20e9e795ae1f77c050c53eb306
Richard Choi <richardchoi54@gmail.com>
2024-02-20 14:50:35 -0500

    updated login function

--- src/hooks/Auth/Login.tsx
+++ src/hooks/Auth/Login.tsx
@@ -6,12 +6,8 @@ import { setEmail } from "../../middleware/variables/Sessions";
 export async function Login(props: AuthInterface) {
   try {
     await api.createSession(props.email, props.password);
-    const response = await api.getAccount();
-    if (response) {
-      console.log(response);
-      setEmail(props.email);
-      window.location.reload();
-    }
+    await api.getAccount();
+    setEmail(props.email);
   } catch (err) {
     console.error(err);
     toast.error(`${err}`);
