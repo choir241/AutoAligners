32852ec92aaa1e0711718a5a25d1265b459bb9cf
richard <choir4558@gmail.com>
2025-07-28 13:07:37 -0400

    update: add labels for app code

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,6 +7,7 @@ import { cacheEmail } from "../../middleware/Cache";
 import "./home.css";
 import { DarkModeContext } from "../../middleware/Context";
 import { useContext } from "react";
+import { labels } from "../../api/labels";
 
 export default function Home() {
   const { toggleDarkMode } = useContext(DarkModeContext);
@@ -16,45 +17,42 @@ export default function Home() {
       {/* {cacheEmail ? (
         <Employee />
       ) : ( */}
-        <main id="hero" className="flex flex-col items-center justify-between">
-          <Nav props={{pageHeading:""}} />
-          <section className={`mx-2 p-4 flex justify-between shadow-2xs ${toggleDarkMode === "dark" ? "light" : "dark"}`}>
+      <main id="hero" className="flex flex-col items-center justify-between">
+        <Nav props={{ pageHeading: "" }} />
+        <section
+          className={`mx-2 p-4 flex justify-between shadow-2xs ${toggleDarkMode === labels.mode.light ? labels.mode.light : labels.mode.dark}`}
+        >
           <div className="w-60">
-              <img
-                src={Assets.whiteCar}
-                alt="white car"
-                className="maxw-full"
-              />
-            </div>
-            
-            <section className="w-60 flex flex-col items-start justify-between">
-              <div>
-                <h2 className="mb-4 text-right">Say Goodbye to Car Repair Hassles</h2>
+            <img
+              src={Assets.whiteCar}
+              alt={labels.home.whiteCarAlt}
+              className="maxw-full"
+            />
+          </div>
 
-                <div className="w-full flex justify-end">
-                <p className="hero-paragraph">
-                  For over a century, dating back to 1892, we have been
-                  steadfast in providing exceptional service. We are determined
-                  to continue building on this legacy with your support.
-                </p>
-                </div>
-  
+          <section className="w-60 flex flex-col items-start justify-between">
+            <div>
+              <h2 className="mb-4 text-right">{labels.home.heroHeadingTwo}</h2>
+
+              <div className="w-full flex justify-end">
+                <p className="hero-paragraph">{labels.home.heroParagraph}</p>
               </div>
-              {cacheEmail
-                ? ButtonLink({
-                    classNames: `mt-6 align-end`,
-                    domain: "/employee",
-                    text: "Employee Hub",
-                  })
-                : ButtonLink({
-                    classNames: `mt-6 align-end`,
-                    domain: "/reservation",
-                    text: "Make Reservation",
-                  })}
-            </section>
+            </div>
+            {cacheEmail
+              ? ButtonLink({
+                  classNames: `mt-6 align-end`,
+                  domain: `/${labels.home.buttonEmployee.domain}`,
+                  text: labels.home.buttonEmployee.text,
+                })
+              : ButtonLink({
+                  classNames: `mt-6 align-end`,
+                  domain: `/${labels.home.buttonReservation.domain}`,
+                  text: labels.home.buttonReservation.text,
+                })}
           </section>
-          <Footer />
-        </main>
+        </section>
+        <Footer />
+      </main>
       {/* )} */}
     </>
   );
